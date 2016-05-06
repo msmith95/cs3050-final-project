0e6325b7a65b2ed3f046462d420e45b067db1102
Michael Smith Jr <mdsf3f@mail.missouri.edu>
2016-05-05 21:45:06 -0500

    Added stuff

--- main.c
+++ main.c
@@ -8,21 +8,45 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "findstrategy.h"
+#include "fileworker.h"
+#include "main.h"
 
 /*
  * 
  */
 int main(int argc, char** argv) {
 
-    int arr[] = {10, 9, 8, 7, 6, 5};
+    int r =  atoi(argv[2]);
+    int n =  atoi(argv[1]);
+    char* outputFile = argv[4];
+    int* inputDays = readfile(argv[3], n);
     int* buyselldays;
-    buyselldays = r1(arr, 6);
-    printf("When r is 1: \n %d\n %d\n~~~~~~~", buyselldays[0] + 1, buyselldays[1] + 1);
+    int numTrades;
+    int i;
+    if(inputDays == NULL){
+        return -5;
+    }
+   if(r == 1){
+         buyselldays = r1(inputDays, n);
+         printFile(outputFile, inputDays, 2);
+         cleanupR1(inputDays, buyselldays);
+    }else if(r > 1){
+         struct BuySellPair* days = rPlus(inputDays, n, r, &numTrades); 
+         printFileR2(outputFile, days, numTrades);
+         cleanupR2(inputDays, days);
+    }
     
-    struct BuySellPair* days = rPlus(arr, 6);
-    printf("When r > 1: \n");
-    for (int i = 0; i < 2; i++)
-          printf(" %d\n %d\n", days[i].buy + 1, days[i].sell + 1);
     return (EXIT_SUCCESS);
+}
+
+
+void cleanupR1(int* input, int* days){
+    free(input);
+    free(days);
+}
+
+void cleanupR2(int* input, struct BuySellPair* days){
+    free(input);
+    free(days);
 }
 
